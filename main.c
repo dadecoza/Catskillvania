6d4f52d7f06b7344fb4e0f5238932500753da6ff
Johannes le Roux <dade@dade.co.za>
2024-04-09 20:04:18 +0200

    smaller window

--- main.c
+++ main.c
@@ -28,7 +28,7 @@ gboolean keyrelease_function(GtkWidget *widget, GdkEventKey *event, gpointer dat
 gboolean main_loop(GtkWidget *widget, GdkFrameClock *clock, gpointer data)
 {
     pb = gdk_pixbuf_new_from_data(playfield, GDK_COLORSPACE_RGB, 0, 8, COLS, ROWS, COLS * BYTES_PER_PIXEL, NULL, NULL);
-    pbs = gdk_pixbuf_scale_simple(pb, COLS * 3, ROWS * 3, GDK_INTERP_BILINEAR);
+    pbs = gdk_pixbuf_scale_simple(pb, COLS * 2, ROWS * 2, GDK_INTERP_BILINEAR);
     gtk_image_set_from_pixbuf(GTK_IMAGE(image), pbs);
     g_object_unref(pb);
     g_object_unref(pbs);
@@ -43,7 +43,7 @@ int main(int argc, char *argv[])
     image = GTK_IMAGE(gtk_image_new());
     GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "Catskillvania");
-    gtk_window_set_default_size(GTK_WINDOW(window), (COLS * 3) + 10, (ROWS * 3) + 10);
+    gtk_window_set_default_size(GTK_WINDOW(window), (COLS * 2) + 10, (ROWS * 2) + 10);
     gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
     GdkPixbuf *icon = gdk_pixbuf_new_from_file("UI/Catskillvania.ico", NULL);
     gtk_window_set_icon(GTK_WINDOW(window), icon);

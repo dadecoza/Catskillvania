54f4cdfab066d597898e69171afd0f1ea66e58be
Johannes le Roux <dade@dade.co.za>
2024-04-16 00:07:12 +0200

    no gif

--- release.sh
+++ release.sh
@@ -5,6 +5,5 @@ rm -rf release && \
 mkdir -p release && \
 ldd catskill.exe | grep '\/mingw.*\.dll' -o | xargs -I{} cp "{}" release && \
 cp -vr audio music sprites condo hallway levels story title UI catskill.exe release && \
-rm -f release/UI/Catskill.gif && \
 make clean
 echo done
